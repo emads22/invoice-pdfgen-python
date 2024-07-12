43035c3e6fb7a38d52e057d73c10de1354aec3d4
Emads <emadsaab722@gmail.com>
2024-07-12 17:01:31 +0300

    update username and email

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ setup(
     name='invoice_pdfgen',  # Package name
     # Automatically find all packages and sub-packages within the directory
     packages=find_packages(),
-    version='1.2.2',  # Package version
+    version='1.2.3',  # Package version
     license='MIT',  # License type
     # Brief description
     description='This package can be used for generating PDF invoices from Excel invoices.',
     long_description=long_description,  # Use README.md content as long description
     # Specify that long description is in Markdown format
     long_description_content_type='text/markdown',
-    author='emads22',  # GitHub username
+    author='Emad',  
     author_email='emadsaab222@gmail.com',  # Author email
     url='https://github.com/emads22/invoice-pdfgen-python',  # GitHub repository URL
     keywords=['invoice', 'excel', 'pdf', 'invoice_pdf_gen',
